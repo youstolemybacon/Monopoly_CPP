f6466e463cf75395254f7d8fdf6e41b44cf31e2c
Zack <zackattack0517@gmail.com>
2024-06-15 16:45:33 -0500

    Updated the draw() function

--- Deck.cpp
+++ Deck.cpp
@@ -7,7 +7,7 @@
 void Deck::shuffle() {
     int swapIndex;
     Cards temp;
-    srand(time(0));
+    srand(time(nullptr)); // Generate random seed for rand() function
 
     while(!discardPile.empty()) {
         swapIndex = rand() % discardPile.size();
@@ -22,120 +22,10 @@ Deck::Cards Deck::draw() {
     if(drawPile.empty()) {
         shuffle();
     }
+    srand(time(nullptr)); // Generate random seed for rand() function
 
-    // Get the current time point with high precision
-    auto currentTime = std::chrono::high_resolution_clock::now();
+    Cards cardDrawn = drawPile.back();
+    drawPile.pop_back();
 
-    // Convert the time point to microseconds
-    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(currentTime.time_since_epoch()).count();
-
-    // Seed the random number generator with the current time in microseconds
-    std::mt19937 generator(static_cast<unsigned int>(microseconds));
-
-    // Generate a random number
-    std::uniform_int_distribution<int> distribution(0, drawPile.size() - 1); // Example distribution
-
-    int cardIndex = distribution(generator);
-    cardList card = deck[cardIndex];
-    deck.erase(deck.begin() + cardIndex);
-
-    displayCard(card, false);
-
-    return card;
-}
-
-Deck::Deck(DeckType deckType) {
-    this->deckType = deckType;
-}
-
-void Deck::displayCard(Deck::cardList card, bool info) {
-    switch (card) {
-        case cardList::AdvanceToGo:
-            cout << "Advance to Go" << endl;
-            break;
-        case cardList::BankError:
-            cout << "Bank Error" << endl;
-            break;
-        case cardList::DoctorsFee:
-            cout << "Doctor's Fee" << endl;
-            break;
-        case cardList::SellStock:
-            cout << "Sell Stock" << endl;
-            break;
-        case cardList::GetOutOfJail:
-            cout << "Get Out of Jail" << endl;
-            break;
-        case cardList::GoToJail:
-            cout << "Go to Jail" << endl;
-            break;
-        case cardList::HolidayFund:
-            cout << "Holiday Fund" << endl;
-            break;
-        case cardList::IncomeTaxRefund:
-            cout << "Income Tax Refund" << endl;
-            break;
-        case cardList::Birthday:
-            cout << "Birthday" << endl;
-            break;
-        case cardList::LifeInsurance:
-            cout << "Life Insurance" << endl;
-            break;
-        case cardList::HospitalFees:
-            cout << "Hospital Fees" << endl;
-            break;
-        case cardList::SchoolFees:
-            cout << "School Fees" << endl;
-            break;
-        case cardList::ReceiveConsultancyFee:
-            cout << "Receive Consultancy Fee" << endl;
-            break;
-        case cardList::StreetRepair:
-            cout << "Street Repair" << endl;
-            break;
-        case cardList::SecondPlaceBeautyContest:
-            cout << "Second Place Beauty Contest" << endl;
-            break;
-        case cardList::Inherit:
-            cout << "Inherit" << endl;
-            break;
-        case cardList::AdvanceToBoardwalk:
-            cout << "Advance to Boardwalk" << endl;
-            break;
-        case cardList::AdvanceToIllinoisAve:
-            cout << "Advance to Illinois Avenue" << endl;
-            break;
-        case cardList::AdvanceToStCharlesPlace:
-            cout << "Advance to St. Charles Place" << endl;
-            break;
-        case cardList::AdvanceToRailroad:
-            cout << "Advance to Railroad" << endl;
-            break;
-        case cardList::AdvanceToUtility:
-            cout << "Advance to Utility" << endl;
-            break;
-        case cardList::BankPaysDividend:
-            cout << "Bank Pays Dividend" << endl;
-            break;
-        case cardList::GoBackThree:
-            cout << "Go Back Three" << endl;
-            break;
-        case cardList::GeneralRepairs:
-            cout << "General Repairs" << endl;
-            break;
-        case cardList::SpeedingFine:
-            cout << "Speeding Fine" << endl;
-            break;
-        case cardList::AdvanceToReadingRailroad:
-            cout << "Advance to Reading Railroad" << endl;
-            break;
-        case cardList::ElectedChairman:
-            cout << "Elected Chairman" << endl;
-            break;
-        case cardList::BuildingLoanMatures:
-            cout << "Building Loan Matures" << endl;
-            break;
-        default:
-            cout << "Unknown card" << endl;
-            break;
-    }
+    return cardDrawn;
 }
