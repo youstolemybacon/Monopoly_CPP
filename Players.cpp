3209d0676ba0ee03e584dfbc6ffe5ca0fd140b5e
Zack_Myers <zackattack0517@gmail.com>
2024-07-10 17:34:14 -0500

    Add class for to deal the multiple players

---
+++ Players.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zacka on 7/10/2024.
+//
+
+#include "Players.h"
---
+++ Players.h
@@ -0,0 +1,19 @@
+//
+// Created by zacka on 7/10/2024.
+//
+
+#ifndef PLAYERS_H
+#define PLAYERS_H
+
+#include <vector>
+#include "Player.h"
+
+
+class Players {
+    vector<Player*> players;
+
+};
+
+
+
+#endif //PLAYERS_H
