b9ea8a98838f35fa731f450ac8a03878273790bf
Zack_Myers <zackattack0517@gmail.com>
2024-07-12 05:32:15 -0500

    Clean up main

--- main.cpp
+++ main.cpp
@@ -5,49 +5,6 @@
 
 using namespace std;
 
-// enum Deck{
-//     Go = 0,
-//     Mediterranean_Avenue = 1,
-//     Community_Chest_1 = 2,
-//     Baltic_Avenue = 3,
-//     Income_Tax = 4,
-//     Reading_Railroad = 5,
-//     Oriental_Avenue = 6,
-//     Chance_1 = 7,
-//     Vermont_Avenue = 8,
-//     Connecticut = 9,
-//     Jail = 10,
-//     St_Charles_Place = 11,
-//     Electric_Company = 12,
-//     States_Avenue = 13,
-//     Virginia_Avenue = 14,
-//     Pennsylvania_Railroad = 15,
-//     St_James_Place = 16,
-//     Community_Chest_2 = 17,
-//     Tennessee_Avenue = 18,
-//     New_York_Avenue = 19,
-//     Free_Parking = 20,
-//     Kentucky_Avenue = 21,
-//     Chance_2 = 22,
-//     Indiana_Avenue = 23,
-//     Illinois_Avenue = 24,
-//     BO_Railroad = 25,
-//     Atlantic_Avenue = 26,
-//     Ventnor_Avenue = 27,
-//     Water_Works = 28,
-//     Marvin_Gardens = 29,
-//     Go_To_Jail = 30,
-//     Pacific_Avenue = 31,
-//     North_Carolina_Avenue = 32,
-//     Community_Chest_3 = 33,
-//     Pennsylvania_Avenue = 34,
-//     Short_Line_Railroad = 35,
-//     Chance_3 = 36,
-//     Park_Place = 37,
-//     Luxury_Tax = 38,
-//     Boardwalk = 39
-// };
-
 void playerSetup(vector<Player*> &players);
 
 int main() {
@@ -55,20 +12,13 @@ int main() {
     playerSetup(players);
     Board board;
 
-    Property* test = dynamic_cast<Property*>(board.getSpace(0));
-    test->displayInfo();
-    players[0]->buyProperty(dynamic_cast<Property*>(board.getSpace(1)));
+    players[0]->buy(dynamic_cast<Property*>(board.getSpace(1)));
+    players[0]->buy(dynamic_cast<Property*>(board.getSpace(0)));
+    players[0]->buy(dynamic_cast<Railroad*>(board.getSpace(2)));
+    dynamic_cast<Property*>(board.getSpace(1))->displayInfo();
     board.getOwnedProperties(players[0]);
     players[0]->printMoney();
-    //players[0].buyProperty();
 
-
-
-
-    //mediterraneanAve.displayPropertyInfo();
-    //cout << mediterraneanAve.getRent() << endl;
-    //mediterraneanAve.build(5);
-    //cout << mediterraneanAve.getRent() << endl;
     return 0;
 }
 
