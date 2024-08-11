aaba05adce4d9da70fbe5da767261e4e0dc18dc4
Zack_Myers <zackattack0517@gmail.com>
2024-08-10 20:46:06 -0500

    Added more demo functions

--- main.cpp
+++ main.cpp
@@ -13,11 +13,18 @@ int main() {
     srand(time(nullptr));
     Players players;
     Property boardwalk(50, 200, 600, 1400, 1700, 2000, 400, 39, "Boardwalk", 400);
+    Utility electric(12, "Electric Company");
     boardwalk.displayInfo();
+    electric.displayInfo();
 
-    Gameplay::getSpaceMenu(Players::playerList[0], Player::playerBoard.getSpace(1));
-    Gameplay::getSpaceMenu(Players::playerList[1], Player::playerBoard.getSpace(5));
+    dynamic_cast<Property*>(Player::playerBoard.getSpace(1))->changeOwner(Players::playerList[0]);
+    dynamic_cast<Property*>(Player::playerBoard.getSpace(3))->changeOwner(Players::playerList[0]);
+    dynamic_cast<Property*>(Player::playerBoard.getSpace(6))->changeOwner(Players::playerList[0]);
+    dynamic_cast<Railroad*>(Player::playerBoard.getSpace(5))->changeOwner(Players::playerList[0]);
+    dynamic_cast<Utility*>(Player::playerBoard.getSpace(28))->changeOwner(Players::playerList[0]);
 
+
+    Players::playerList[0]->printSpaces();
 
     return 0;
 }
