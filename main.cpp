15b7a11697403527ba78e58ffef34978c06accc7
Zack_Myers <zackattack0517@gmail.com>
2024-08-13 22:10:36 -0500

    Update testing in main()

--- main.cpp
+++ main.cpp
@@ -12,19 +12,28 @@ class Gameplay;
 int main() {
     srand(time(nullptr));
     Players players;
+    Gameplay game;
     Property boardwalk(50, 200, 600, 1400, 1700, 2000, 400, 39, "Boardwalk", 400);
     Utility electric(12, "Electric Company");
     boardwalk.displayInfo();
     electric.displayInfo();
 
-    dynamic_cast<Property*>(Player::playerBoard.getSpace(1))->changeOwner(Players::playerList[0]);
-    dynamic_cast<Property*>(Player::playerBoard.getSpace(3))->changeOwner(Players::playerList[0]);
-    dynamic_cast<Property*>(Player::playerBoard.getSpace(6))->changeOwner(Players::playerList[0]);
-    dynamic_cast<Railroad*>(Player::playerBoard.getSpace(5))->changeOwner(Players::playerList[0]);
-    dynamic_cast<Utility*>(Player::playerBoard.getSpace(28))->changeOwner(Players::playerList[0]);
+    dynamic_cast<Property*>(Player::playerBoard.getSpace(1))->changeOwner(Players::playerList[1]);
+    dynamic_cast<Property*>(Player::playerBoard.getSpace(3))->changeOwner(Players::playerList[1]);
+    dynamic_cast<Property*>(Player::playerBoard.getSpace(6))->changeOwner(Players::playerList[1]);
+    dynamic_cast<Railroad*>(Player::playerBoard.getSpace(5))->changeOwner(Players::playerList[1]);
+    dynamic_cast<Railroad*>(Player::playerBoard.getSpace(15))->changeOwner(Players::playerList[1]);
+    dynamic_cast<Railroad*>(Player::playerBoard.getSpace(25))->changeOwner(Players::playerList[1]);
+    dynamic_cast<Railroad*>(Player::playerBoard.getSpace(35))->changeOwner(Players::playerList[1]);
+    dynamic_cast<Utility*>(Player::playerBoard.getSpace(28))->changeOwner(Players::playerList[1]);
 
 
     Players::playerList[0]->printSpaces();
+
+    while(true)
+    {
+        game.turn(Players::playerList[0]);
+    }
 
     return 0;
 }
