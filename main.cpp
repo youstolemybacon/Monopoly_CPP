9ac1a669ff757e25b656cc691521533639e9fdc3
Zack_Myers <zackattack0517@gmail.com>
2024-08-25 17:15:08 -0500

    Removed some of the code previously used for testing

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "Board.h"
 #include "Dice.h"
 #include "Gameplay.h"
-
 #include "Players.h"
 
 using namespace std;
@@ -17,24 +16,11 @@ int main() {
     Players::playerList[1]->income(3000);
     dynamic_cast<Property*>(Player::playerBoard.getSpace(1))->buy(Players::playerList[1]);
     dynamic_cast<Property*>(Player::playerBoard.getSpace(3))->buy(Players::playerList[1]);
-    dynamic_cast<Property*>(Player::playerBoard.getSpace(6))->buy(Players::playerList[1]);
     dynamic_cast<Railroad*>(Player::playerBoard.getSpace(5))->buy(Players::playerList[1]);
-    dynamic_cast<Railroad*>(Player::playerBoard.getSpace(15))->buy(Players::playerList[1]);
-    dynamic_cast<Railroad*>(Player::playerBoard.getSpace(25))->buy(Players::playerList[1]);
     dynamic_cast<Railroad*>(Player::playerBoard.getSpace(35))->buy(Players::playerList[1]);
     dynamic_cast<Utility*>(Player::playerBoard.getSpace(12))->buy(Players::playerList[1]);
-    dynamic_cast<Utility*>(Player::playerBoard.getSpace(28))->buy(Players::playerList[1]);
     dynamic_cast<Property*>(Player::playerBoard.getSpace(39))->buy(Players::playerList[0]);
     dynamic_cast<Property*>(Player::playerBoard.getSpace(37))->buy(Players::playerList[0]);
-
-    cout << "Monopoly: " << Property::monopolyCheck(Players::playerList[1], Property::PropertyGroup::BROWN) << endl;
-    cout << "Rent: " << dynamic_cast<Property*>(Player::playerBoard.getSpace(1))->getRent() << endl;
-    cout << "Monopoly: " << Property::monopolyCheck(Players::playerList[1], Property::PropertyGroup::BLUE) << endl;
-    cout << "Rent: " << dynamic_cast<Property*>(Player::playerBoard.getSpace(39))->getRent() << endl;
-
-    OwnableSpaces::spaceInfoMenu(Board::getOwnedSpaces(Players::playerList[0]));
-
-    Property::developPropertiesMenu(Players::playerList[1]);
 
     while(true)
     {
